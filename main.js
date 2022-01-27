227f5b57db4e1567dcd2931e67b141238943c20c
iLuckyW <98313436+iLuckyW@users.noreply.github.com>
2022-01-27 09:09:10 +0100

    tick

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 let fields = document.querySelectorAll(".field")
 let moleIndex = 0;
 
-setInterval("moveMole()", 2000)
+//setInterval("tick()", 500)
+
+function tick() {
+    x = getRandomInt(4)
+    
+    if (x == 0) {
+        // Maulwurf entfernen
+        setMole(-1)
+    } else if (x == 1) {
+        // Maulwurf bewegen
+        moveMole()
+    } else {
+        // Nichts tun
+    }
+}
 
 function moveMole() {
+    // Bewegt den Maulwurf zu einer zufaelligen (neuen) Position
     x = getRandomInt(8)
 
     if (moleIndex == x) {
@@ -14,6 +29,8 @@ function moveMole() {
 }
 
 function setMole(index) {
+    // Bewegt den Maulwurf zu der im Parameter gegebenen Position
+    // Wenn der Inex außerhalb des Definitinsbereiches liegt wird der Maulwurf nur entfernt
     fields[moleIndex].classList.remove('mole')
 
     if (index < fields.length && index >= 0) {
@@ -23,5 +40,6 @@ function setMole(index) {
 }
 
 function getRandomInt(max) {
+    // Gibt eine Zufallszahl von 0 bis zum Parameter
     return Math.floor(Math.random() * max);
 }
