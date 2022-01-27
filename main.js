e7586f18306a363c310156ba7c2d1af95c46b251
iLuckyW <98313436+iLuckyW@users.noreply.github.com>
2022-01-27 11:20:18 +0100

    ui

--- main.js
+++ main.js
@@ -1,19 +1,47 @@
 let fields = document.querySelectorAll(".field")
-let moleIndex = 0;
+let moleIndex = 0
+let points = 0
+let counter = 50
 
-//setInterval("tick()", 500)
+setInterval("tick()", 300)
+
+document.onclick = function(event) {
+    // Index des angeklickten Feldes finden
+    i = 0;
+    while (i < fields.length && fields[i] != event.target) {
+        i++
+    }
+
+    // Falls Maulwurf da, entfernen und Punkt hinzufuegen
+    if (i < fields.length && i == moleIndex) {
+        setMole(-1)
+        points += 1
+        document.getElementById("points").innerHTML = points
+    }
+
+    if (event.target == ocument.querySelectorAll(".start")) {
+        alert("click")
+    }
+};
 
 function tick() {
-    x = getRandomInt(4)
+    if (counter < 50) {
+        x = getRandomInt(4)
     
-    if (x == 0) {
-        // Maulwurf entfernen
-        setMole(-1)
-    } else if (x == 1) {
-        // Maulwurf bewegen
-        moveMole()
+        if (x == 0) {
+            // Maulwurf entfernen
+            setMole(-1)
+        } else if (x == 1) {
+            // Maulwurf bewegen
+            moveMole()
+        } else {
+            // Nichts tun
+        }
+
+        counter += 1;
+        document.getElementById("sec").innerHTML = 15.0 - (counter * 0.3)
     } else {
-        // Nichts tun
+        setMole(-1)
     }
 }
 
@@ -30,12 +58,13 @@ function moveMole() {
 
 function setMole(index) {
     // Bewegt den Maulwurf zu der im Parameter gegebenen Position
-    // Wenn der Inex außerhalb des Definitinsbereiches liegt wird der Maulwurf nur entfernt
-    fields[moleIndex].classList.remove('mole')
+    if (moleIndex != -1) fields[moleIndex].classList.remove('mole')
 
     if (index < fields.length && index >= 0) {
         moleIndex = index
         fields[index].classList.add('mole')
+    } else {
+        moleIndex = -1
     }
 }
 
